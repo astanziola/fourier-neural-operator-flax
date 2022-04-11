5cd9bb09754079126c8822548661b132b331018f
Antonio Stanziola <stanziola.antonio@gmail.com>
2022-04-11 16:06:24 +0100

    feat: FNO (untested)

---
+++ README.md
@@ -0,0 +1,5 @@
+# Fourier Neural Operator (Flax)
+
+This is an **unofficial** [Flax]() ([JAX]()) port of the [Fourier Neural Operator](https://arxiv.org/abs/2010.08895) developed by Zongyi Li et al.
+
+Please visit [this repository](https://github.com/zongyi-li/fourier_neural_operator) to access the official PyTorch implementation of FNO form the paper's authors, and for the citation policy in case you use FNO in your research.
---
+++ setup.py
@@ -0,0 +1,45 @@
+"""Python setup.py for fno package"""
+import io
+import os
+
+from setuptools import find_packages, setup
+
+
+def read(*paths, **kwargs):
+  """Read the contents of a text file safely.
+  >>> read("fno", "VERSION")
+  '0.1.0'
+  >>> read("README.md")
+  ...
+  """
+
+  content = ""
+  with io.open(
+    os.path.join(os.path.dirname(__file__), *paths),
+    encoding=kwargs.get("encoding", "utf8"),
+  ) as open_file:
+    content = open_file.read().strip()
+  return content
+
+
+def read_requirements(path):
+  return [
+    line.strip()
+    for line in read(path).split("\n")
+    if not line.startswith(('"', "#", "-", "git+"))
+  ]
+
+setup(
+    name="fno",
+    version=read("fno", "VERSION"),
+    description="Fourier Neural Operator",
+    url="https://github.com/astanziola/fno-jax",
+    long_description=read("README.md"),
+    long_description_content_type="text/markdown",
+    author="Antonio Stanziola, UCL BUG",
+    author_email="a.stanziola@ucl.ac.uk",
+    packages=find_packages(exclude=["tests", ".github", "docs"]),
+    python_requires=">=3.7",
+    install_requires=read_requirements("requirements.txt"),
+    license="MIT License"
+)
